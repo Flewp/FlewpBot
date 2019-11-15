b6c789d3ff3402485f1c64b18f80728af7905414
Alex Hendrix <alex@flewp.net>
2019-11-15 17:50:16 -0600

    Adding play immediately functionality to requests

--- src/main/java/com/flewp/flewpbot/model/api/JamisphereRequestBehalfBody.java
+++ src/main/java/com/flewp/flewpbot/model/api/JamisphereRequestBehalfBody.java
@@ -4,10 +4,12 @@ public class JamisphereRequestBehalfBody {
     public String request;
     public String behalfOfUserName;
     public Boolean vip;
+    public Boolean playInstantly;
 
-    public JamisphereRequestBehalfBody(String request, String behalfOfUserName, Boolean vip) {
+    public JamisphereRequestBehalfBody(String request, String behalfOfUserName, Boolean vip, Boolean playInstantly) {
         this.request = request;
         this.behalfOfUserName = behalfOfUserName;
         this.vip = vip;
+        this.playInstantly = playInstantly;
     }
 }
