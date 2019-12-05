9b6b82181165ee67a7fee7140c3ddf5a8e93d409
Alex Hendrix <alex@flewp.net>
2019-12-05 00:53:55 -0600

    Fixing answered event

--- src/main/java/com/flewp/flewpbot/model/events/jamisphere/GuessingGameAnsweredEvent.java
+++ src/main/java/com/flewp/flewpbot/model/events/jamisphere/GuessingGameAnsweredEvent.java
@@ -7,7 +7,7 @@ import com.flewp.flewpbot.model.jamisphere.JamisphereUser;
 import com.github.philippheuer.events4j.domain.Event;
 
 public class GuessingGameAnsweredEvent extends Event {
-    public JamisphereUser user;
+    public JamisphereUser winner;
     public JamisphereRequest request;
     public JamisphereGuess guess;
     public JamisphereGuessingGame guessingGame;
