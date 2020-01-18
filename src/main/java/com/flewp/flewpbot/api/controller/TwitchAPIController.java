da6f129c1d5e22737952b287152d7706e347b1b0
Alex Hendrix <alex@flewp.net>
2020-01-17 23:57:56 -0600

    Logging disconnects, but allowing bot to reconnect itself

--- src/main/java/com/flewp/flewpbot/api/controller/TwitchAPIController.java
+++ src/main/java/com/flewp/flewpbot/api/controller/TwitchAPIController.java
@@ -182,27 +182,18 @@ public class TwitchAPIController {
     }
 
     @Handler
-    public void onDisconnect(ChannelPartEvent event) {
-        LoggerFactory.getLogger(TwitchAPIController.class).info("ChannelPartEvent, attempting reconnect." + event.toString());
-        ircClient.reconnect();
-    }
-
-    @Handler
     public void onDisconnect(ClientConnectionClosedEvent event) {
-        LoggerFactory.getLogger(TwitchAPIController.class).info("ClientConnectionClosedEvent, attempting reconnect." + event.toString());
-        ircClient.reconnect();
+        LoggerFactory.getLogger(TwitchAPIController.class).info(event.toString());
     }
 
     @Handler
     public void onDisconnect(ClientConnectionEndedEvent event) {
-        LoggerFactory.getLogger(TwitchAPIController.class).info("ClientConnectionEndedEvent, attempting reconnect." + event.toString());
-        ircClient.reconnect();
+        LoggerFactory.getLogger(TwitchAPIController.class).info(event.toString());
     }
 
     @Handler
     public void onDisconnect(ClientConnectionFailedEvent event) {
-        LoggerFactory.getLogger(TwitchAPIController.class).info("ClientConnectionFailedEvent, attempting reconnect." + event.toString());
-        ircClient.reconnect();
+        LoggerFactory.getLogger(TwitchAPIController.class).info(event.toString());
     }
 
     public String getStreamerUserId() {
