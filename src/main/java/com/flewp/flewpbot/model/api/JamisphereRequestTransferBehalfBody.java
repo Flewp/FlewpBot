aad9e4e7629a53655eb5e8ff33ffdf484621cbb6
Alex Hendrix <alex@flewp.net>
2020-04-09 11:10:07 -0500

    Adding constructor to transfer body

--- src/main/java/com/flewp/flewpbot/model/api/JamisphereRequestTransferBehalfBody.java
+++ src/main/java/com/flewp/flewpbot/model/api/JamisphereRequestTransferBehalfBody.java
@@ -3,4 +3,9 @@ package com.flewp.flewpbot.model.api;
 public class JamisphereRequestTransferBehalfBody {
     public String fromUserName;
     public String toUserName;
+
+    public JamisphereRequestTransferBehalfBody(String fromUserName, String toUserName) {
+        this.fromUserName = fromUserName;
+        this.toUserName = toUserName;
+    }
 }
