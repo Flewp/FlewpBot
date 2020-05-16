b338870785bc97598ab95c20f548b2bb497bca0e
Alex Hendrix <alex@flewp.net>
2020-05-16 17:47:27 -0500

    Adding util methods to event start

--- src/main/java/com/flewp/flewpbot/model/api/JamisphereEventStartBody.java
+++ src/main/java/com/flewp/flewpbot/model/api/JamisphereEventStartBody.java
@@ -1,5 +1,6 @@
 package com.flewp.flewpbot.model.api;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class JamisphereEventStartBody {
@@ -14,8 +15,37 @@ public class JamisphereEventStartBody {
     public String choiceType;
     public List<String> choices;
 
-    String rewardName;
-    String rewardPrompt;
-    String rewardUserInput;
-    Integer rewardCost;
+    public String rewardName;
+    public String rewardUserInput;
+
+    public static JamisphereEventStartBody startGuessingGame(String requestId, Integer length) {
+        JamisphereEventStartBody jamisphereEventStartBody = new JamisphereEventStartBody();
+
+        jamisphereEventStartBody.type = "guessingGame";
+        jamisphereEventStartBody.requestId = requestId;
+        jamisphereEventStartBody.length = length;
+
+        return jamisphereEventStartBody;
+    }
+
+    public static JamisphereEventStartBody startChoiceGame(boolean isSongChoices, List<String> choices, Integer length) {
+        JamisphereEventStartBody jamisphereEventStartBody = new JamisphereEventStartBody();
+
+        jamisphereEventStartBody.type = "multipleChoice";
+        jamisphereEventStartBody.choiceType = isSongChoices ? "song" : "other";
+        jamisphereEventStartBody.length = length;
+        jamisphereEventStartBody.choices = new ArrayList<>(choices);
+
+        return jamisphereEventStartBody;
+    }
+
+    public static JamisphereEventStartBody startClaimGame(String rewardName, String rewardUserInput) {
+        JamisphereEventStartBody jamisphereEventStartBody = new JamisphereEventStartBody();
+
+        jamisphereEventStartBody.type = "claim";
+        jamisphereEventStartBody.rewardName = rewardName;
+        jamisphereEventStartBody.rewardUserInput = rewardUserInput;
+
+        return jamisphereEventStartBody;
+    }
 }
